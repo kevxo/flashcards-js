f2888daaf620ac8eb3d316ab8fb9c1a8c7d4a09e
Kevin Cuadros <kev040798@gmail.com>
2021-06-10 21:21:48 -0400

    Completes iteration 3 with passing test and game running correctly

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,30 @@
 const data = require('./data');
+const Round = require('./Round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = {};
+  }
+
+  start(){
+    const cards = prototypeQuestions.map(function(cardInfo){
+      let card = new Card(cardInfo.id, cardInfo.question, cardInfo.answers, cardInfo.correctAnswer);
+
+      return card;
+    });
+
+
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck)
+    const round = this.currentRound;
+
+    this.printMessage(deck, round);
+    this.printQuestion(round);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
